53bb43e6b6064226c6e2097a1ebc6a30d6ced63e
arthur <gogoke1@naver.com>
2019-02-20 01:22:38 +0900

    컨트롤러 수정

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,13 @@ public class MainController {
     UserService userService;
 
     @GetMapping(value = "/{id}")
-    public User main(@PathVariable Long id){
-        return userService.getUser(id);
+    public ResponseEntity<User> main(@PathVariable Long id){
+        User user = userService.getUser(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/test")
+    public String test(){
+        return "{\"a\":\"bb\"}";
     }
 }
