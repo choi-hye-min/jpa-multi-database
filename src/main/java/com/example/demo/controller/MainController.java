06bf272bf851e571aea919da0176f422f0623b0d
gogoke1 <gogoke1@within.co.kr>
2019-02-20 09:20:31 +0900

    entity modify

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,10 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +20,7 @@ public class MainController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> main(@PathVariable Long id){
         User user = userService.getUser(id);
+
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,11 +1,19 @@
 package com.example.demo.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Table(name = "user")
+@EntityListeners(value = {AuditingEntityListener.class})
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -19,4 +27,14 @@ public class User {
 
     @Column
     private LocalDateTime createAt;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", createAt=" + createAt +
+                '}';
+    }
 }
